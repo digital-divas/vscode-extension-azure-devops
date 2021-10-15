c6cb22df8cb251a14448f3610bdb8571d48c5b1f
Ezequiel Ramos <Ezequiel Ramos>
2021-10-15 20:29:18 -0300

    adding login and create pull request options

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,143 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { DepNodeProvider, Dependency } from './nodeDependencies';
+import * as azdev from "azure-devops-node-api";
+import * as open from 'open';
+import { DepNodeProvider } from './nodeDependencies';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "azure-devops" is now active!');
+	let azurePAT: string | undefined;
+	let azureOrg: string | undefined;
+	let azureProject: string | undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('azure-devops.helloWorld', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const value = await vscode.window.showInputBox();
-		console.log(value);
-		vscode.window.showInformationMessage('Hello World from Azure DevOps!');
+	const azurePATKey = context.extension.id + '.azure_pat';
+	const azureOrgKey = context.extension.id + '.azure_org_key';
+	const azureProjectKey = context.extension.id + '.azure_project_key';
+	context.globalState.setKeysForSync([azurePATKey, azureOrgKey, azureProjectKey]);
+
+	azurePAT = context.globalState.get(azurePATKey);
+	azureOrg = context.globalState.get(azureOrgKey);
+	azureProject = context.globalState.get(azureProjectKey);
+
+	console.log("azurePAT", azurePAT);
+	console.log("azureOrgKey", azureOrg);
+	console.log("azureProject", azureProject);
+
+	const login = vscode.commands.registerCommand('azure-devops.login', async () => {
+
+		let input;
+
+		input = await vscode.window.showInputBox({
+			title: "Azure Organization URI",
+			placeHolder: "Azure Organization URI",
+			value: "https://dev.azure.com/yourorganization",
+		});
+
+		if (input) {
+			azureOrg = input;
+			context.globalState.update(azureOrgKey, azureOrg);
+		}
+
+		input = await vscode.window.showInputBox({
+			title: "Azure Project Name",
+			placeHolder: "Azure Project Name"
+		});
+
+		if (input) {
+			azureProject = input;
+			context.globalState.update(azureProjectKey, azureProject);
+		}
+
+		input = await vscode.window.showInputBox({
+			title: "Azure DevOps Personal Access Token",
+			placeHolder: "Azure DevOps Personal Access Token"
+		});
+
+		if (input) {
+			azurePAT = input;
+			context.globalState.update(azurePATKey, azurePAT);
+		}
+
 	});
+
+	const createPullRequest = vscode.commands.registerCommand('azure-devops.createPullRequest', async () => {
+
+		if (!azureOrg || !azureProject || !azurePAT) {
+			vscode.window.showWarningMessage('You need to login to Azure DevOps first.');
+			return;
+		}
+
+		const repositoryName = await vscode.window.showInputBox({
+			title: "Repository Name",
+			placeHolder: "Repository Name",
+			value: "zowee-firmware"
+		});
+
+		if (!repositoryName) {
+			return;
+		}
+
+		const sourceBranch = await vscode.window.showInputBox({
+			title: "Source Branch",
+			placeHolder: "Source Branch",
+			value: "stage"
+		});
+
+		if (!sourceBranch) {
+			return;
+		}
+
+		const targetBranch = await vscode.window.showInputBox({
+			title: "Target Branch",
+			placeHolder: "Target Branch",
+			value: "stage"
+		});
+
+		if (!targetBranch) {
+			return;
+		}
+
+		try {
+			let authHandler = azdev.getPersonalAccessTokenHandler(azurePAT);
+			let connection = new azdev.WebApi(azureOrg, authHandler);
+
+			let git = await connection.getGitApi();
+
+			const repositories = await git.getRepositories(azureProject, false, false, false);
+			const repository = repositories.filter((each) => each.name === repositoryName)[0];
+
+			if (repository.id) {
+				const gitpullrequest = await git.createPullRequest({
+					targetRefName: "refs/heads/" + targetBranch,
+					sourceRefName: "refs/heads/" + sourceBranch,
+					title: sourceBranch,
+					repository: repository,
+				}, repository.id, azureProject, true);
+				const link = gitpullrequest.repository?.webUrl + "/pullrequest/" + gitpullrequest.pullRequestId;
+				const thenable = await vscode.window.showInformationMessage("Pull request succefully created! ", {}, { title: "Open on Browser" });
+				if (thenable && thenable.title === "Open on Browser") {
+					open(link);
+				}
+
+			}
+
+		} catch (err) {
+			console.error(err);
+			vscode.window.showErrorMessage((err as Error).message);
+		}
+
+	});
+
+	context.subscriptions.push(login);
+	context.subscriptions.push(createPullRequest);
 
 	const nodeDependenciesProvider = new DepNodeProvider("/mnt/5cb80605-c86e-4351-b941-96d1cb6eadab/Documents/Projects/vscode-extension-azure-devops");
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 
-	// vscode.window.createTreeView("nodeDependencies", { treeDataProvider: nodeDependenciesProvider, canSelectMany: false, showCollapseAll: false });
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
